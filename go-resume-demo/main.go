eaee2515d78db4a09e67a46551d40316cafada95
woliekailiao <744725502@qq.com>
2025-09-27 01:39:05 +0800

    feat: Day-5 unified response + custom error codes + global error handler

--- go-resume-demo/main.go
+++ go-resume-demo/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/Cyte-091/go-resume-demo/initialize"
 	"github.com/Cyte-091/go-resume-demo/middleware"
 	"github.com/Cyte-091/go-resume-demo/router"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -18,6 +19,15 @@ func main() {
 	_ = docs.SwaggerInfo
 	initialize.InitGorm()
 	r := gin.New()
+	// 1. 跨域拦截（必须放最前）
+	// 1. 显式 CORS 配置（必须放最前）
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:8081"}, // 前端地址
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"}, // 允许带 Authorization
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 	r.Use(middleware.ErrorHandler()) // ← 全局异常
 	r.Use(gin.Recovery(), gin.Logger())
 	router.Setup(r) // ← 唯一入口
